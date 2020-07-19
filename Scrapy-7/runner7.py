85e431b6c12b0c43a9fab73c6f939dd6b715734e
sonofthe90s <60989074+sonofthe90s@users.noreply.github.com>
2020-07-20 01:03:38 +0300

    Add files via upload

---
+++ Scrapy-7/runner7.py
@@ -0,0 +1,13 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+
+from leruaparser import settings
+from leruaparser.spiders.lerua import LeruaSpider
+
+if __name__ == '__main__':
+    crawler_settings = Settings()
+    crawler_settings.setmodule(settings)
+
+    process = CrawlerProcess(settings=crawler_settings)
+    process.crawl(LeruaSpider, search='газонокосилка')
+    process.start()
